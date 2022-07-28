7e7bc56cce7a7f95202b7dce550fb621da3021da
André Lubawski <andrelubawski97@outlook.com>
2022-07-28 01:59:31 -0300

    js basico - ate 0202

---
+++ JSbasico/Variaveis/script.js
@@ -0,0 +1,21 @@
+//console.log('oi');
+var nome = "André";
+var idade = 25;
+var possuiFaculdade = false;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado*preco;
+
+console.log(totalPreco);
+
+var sobrenome = "Lubawski", 
+    cidade = "Massaranduba";
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+
---
+++ JSbasico/Variaveis/script2.js
@@ -0,0 +1,16 @@
+var nome = "André Lubawski"
+console.log(typeof nome);
+var time = null;
+console.log(typeof time);
+var simbolo=Symbol()
+console.log(typeof simbolo);
+
+var ano = '2021';
+console.log(ano+1);
+
+//template string
+
+var gols = 1000;
+
+var frase = `Romário fez ${gols*3} gols`;
+console.log(frase);

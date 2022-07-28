ffedc1df62e40779e5213d4db97b4ed5b356baa8
André Lubawski <andrelubawski97@outlook.com>
2022-07-28 07:08:36 -0300

    aula 0203

---
+++ JSbasico/Variaveis/script3.js
@@ -0,0 +1,21 @@
+var idade =2e2;
+console.log(idade);
+
+var exp = 2**4;
+console.log(exp);
+
+var testeNaN = 'Isso é 100' /2;
+console.log(isNaN(testeNaN));
+
+var testeNaN2 = '100' /2;
+console.log(isNaN(testeNaN2));
+
+var x = 1;
+console.log(x++);
+console.log(x);
+console.log(++x);
+console.log(--x);
+
+var idade='30';
+console.log(+idade + 1);
+console.log(-idade + 1);

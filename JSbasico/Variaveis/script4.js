e6c4ca68647001c77df12a64ef145f43e87d2e0d
André Lubawski <andrelubawski97@outlook.com>
2022-07-30 05:09:28 -0300

    0204-boolean-condicionais-1

---
+++ JSbasico/Variaveis/script4.js
@@ -0,0 +1,10 @@
+var possuiGraduacao = false;
+var possuiDoutorado = true;
+
+if(possuiGraduacao){
+    console.log('possui graduação');
+}else if(possuiDoutorado){
+    console.log('possui doutorado');
+}else{
+    console.log("não possui nada");
+}

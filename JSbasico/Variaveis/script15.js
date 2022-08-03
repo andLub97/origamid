0a25c93628c0978f155676c356ed9fe9087829ce
André Lubawski <andrelubawski97@outlook.com>
2022-08-02 21:23:51 -0300

    arrays

---
+++ JSbasico/Variaveis/script15.js
@@ -0,0 +1,11 @@
+var videoGames = ['Switch', 'PS4','XBox'];
+
+console.log(videoGames.pop());
+console.log(videoGames);
+console.log(videoGames.push('3DS'));
+console.log(videoGames);
+
+
+for(var n = 0; n < 3;n++){
+    console.log(n);
+}

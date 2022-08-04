68c9728bc719b83d7fca9732a2d5fc0b44e93a59
André Lubawski <andrelubawski97@outlook.com>
2022-08-04 13:14:21 -0300

    ternário e escopo

---
+++ JSbasico/Variaveis/script17.js
@@ -0,0 +1,8 @@
+var numero = 20;
+
+numero /= 10;
+console.log(numero);
+
+var idade = 17;
+var podeBeber = (idade>=18)? 'Pode beber' : 'não pode beber';
+console.log(podeBeber);
---
+++ JSbasico/Variaveis/script18.js
@@ -0,0 +1,6 @@
+var i = 50;
+
+for(let i = 0; i < 10; i++){
+    console.log(`Número ${i}`);
+}
+console.log(i*10);
